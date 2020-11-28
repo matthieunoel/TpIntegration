aef9cc5d38bc6aedbc327c36fe9bf72cf55154e9
Thomas <44287699+Poussinators@users.noreply.github.com>
2020-11-28 20:30:13 +0100

    Modif + essai de test fonction CreateTableClient

--- src/__tests__/root.spec.js
+++ src/__tests__/root.spec.js
@@ -1,14 +1,35 @@
 const RootService = require("../root/root.service").RootService;
 const AppConfig = require("../app").AppConfig;
 const fs = require('fs')
+const Database = require('better-sqlite3');
+const dbPath = '<rootDir>/db/SQLiteTest.db';
+
+function DeleteFileIfExists (filePath) {
+    if (fs.existsSync(filePath)) {
+        fs.unlink(filePath);
+    }
+}
+
 
 describe('AppComponent', () => {
 
-    it('should have total equal 0', () => {
+    it('Devrais avoir créer la base de donnée', () => {
+        // DeleteFileIfExists(AppConfig.dbPath);
         const rootService = new RootService();
         rootService.CreateDbFileIfNotExists(AppConfig.dbPath);
         const fileExist = fs.existsSync(AppConfig.dbPath);
         expect(fileExist).toBe(true);
+    });
+
+    it('Devrais avoir créer la table CLient', () => {
+        // DeleteFileIfExists(dbPath);
+        const rootService = new RootService();
+        rootService.CreateDbFileIfNotExists(AppConfig.dbPath);
+        const fileExist = fs.existsSync(AppConfig.dbPath);
+        rootService.CreateTableClient(AppConfig.dbPath);
+        const request = 'DESCRIBE CLIENT;';
+        var test = new Database(AppConfig.dbPath).prepare(request).run();
+        expect(test).toBe(true);
     });
 
 });
