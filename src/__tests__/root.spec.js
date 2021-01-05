a21f0b2f711fcdeb31da1765e6167cb4ecacb52b
MattUnicornBlue <matthieu.noel@epsi.fr>
2021-01-05 16:17:11 +0100

    Là on avance ptn !

--- src/__tests__/root.spec.js
+++ src/__tests__/root.spec.js
@@ -4,32 +4,34 @@ const fs = require('fs')
 const Database = require('better-sqlite3');
 const dbPath = '<rootDir>/db/SQLiteTest.db';
 
-function DeleteFileIfExists (filePath) {
+function DeleteFileIfExists(filePath) {
     if (fs.existsSync(filePath)) {
-        fs.unlink(filePath);
+        fs.unlinkSync(filePath);
     }
 }
 
+testDbPath = './db/test.db'
 
 describe('AppComponent', () => {
 
     it('Devrais avoir créer la base de donnée', () => {
-        // DeleteFileIfExists(AppConfig.dbPath);
+        DeleteFileIfExists(testDbPath);
         const rootService = new RootService();
-        rootService.CreateDbFileIfNotExists(AppConfig.dbPath);
-        const fileExist = fs.existsSync(AppConfig.dbPath);
+        rootService.CreateDbFileIfNotExists(testDbPath);
+        const fileExist = fs.existsSync(testDbPath);
         expect(fileExist).toBe(true);
     });
 
-    it('Devrais avoir créer la table CLient', () => {
-        // DeleteFileIfExists(dbPath);
+    it('Devrais avoir créer la table Client', () => {
+        DeleteFileIfExists(dbPath);
         const rootService = new RootService();
-        rootService.CreateDbFileIfNotExists(AppConfig.dbPath);
-        const fileExist = fs.existsSync(AppConfig.dbPath);
-        rootService.CreateTableClient(AppConfig.dbPath);
-        const request = 'DESCRIBE CLIENT;';
-        var test = new Database(AppConfig.dbPath).prepare(request).run();
-        expect(test).toBe(true);
+        rootService.CreateDbFileIfNotExists(testDbPath);
+        rootService.CreateTableClient(testDbPath);
+        // Si aucune erreur ne pète, c'est que la table existe.
+        // On pends l'id -1 car aucune client n'as pour id le -1, ce qui va potentiellement accélérer la requête/
+        const request = 'SELECT * FROM CLIENT WHERE id=-1;';
+        var res = new Database(testDbPath).prepare(request).get();
+        expect(res).toBe(undefined);
     });
 
 });
